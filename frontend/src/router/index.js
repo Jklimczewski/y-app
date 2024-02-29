389c202f3eae647ccc63973657a4433b56212695
Jakub Klimczewski <kukli8@o2.pl>
2024-02-29 11:57:59 +0100

    Store changes

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -4,6 +4,61 @@ import Login from "../pages/Login.vue";
 import Register from "../pages/Register.vue";
 import NotFound from "../pages/NotFound.vue";
 import { useUserStore } from "../stores/userStore";
+import DataService from "../services/DataService";
+
+function isAuthenticated(to, from, next) {
+  const store = useUserStore();
+  if (!store.getEmail) {
+    DataService.getData()
+      .then((res) => {
+        store.saveUser(
+          res.data.user._id,
+          res.data.user.username,
+          res.data.user.follows,
+          res.data.user.profilePicture,
+          res.data.user.name,
+          res.data.user.surname,
+          res.data.user.phoneNumber,
+          res.data.user.email
+        );
+        next();
+      })
+      .catch((err) => {
+        if (err.response && err.response.status == 401) {
+          next("/login");
+        }
+      });
+  } else {
+    next();
+  }
+}
+
+function isAlreadyLoggedIn(to, from, next) {
+  const store = useUserStore();
+  if (!store.getEmail) {
+    DataService.getData()
+      .then((res) => {
+        store.saveUser(
+          res.data.user._id,
+          res.data.user.username,
+          res.data.user.follows,
+          res.data.user.profilePicture,
+          res.data.user.name,
+          res.data.user.surname,
+          res.data.user.phoneNumber,
+          res.data.user.email
+        );
+        next("/profile");
+      })
+      .catch((err) => {
+        if (err.response && err.response.status == 401) {
+          next();
+        }
+      });
+  } else {
+    next("/profile");
+  }
+}
 
 const routes = [
   { path: "/", component: Home },
@@ -11,56 +66,38 @@ const routes = [
     path: "/login",
     name: "Login",
     component: Login,
-    beforeEnter: () => {
-      const store = useUserStore();
-      if (store.getUser != "") return "/profile";
-    },
+    beforeEnter: [isAlreadyLoggedIn],
   },
   {
     path: "/register",
     name: "Register",
     component: Register,
-    beforeEnter: () => {
-      const store = useUserStore();
-      if (store.getUser != "") return "/profile";
-    },
+    beforeEnter: [isAlreadyLoggedIn],
   },
   {
     path: "/profile",
     name: "Profile",
     component: () => import("../pages/profile/Profile.vue"),
-    beforeEnter: () => {
-      const store = useUserStore();
-      if (store.getUser == "") return "/login";
-    },
+    beforeEnter: [isAuthenticated],
   },
   {
     path: "/posts",
     name: "NewPosts",
     component: () => import("../pages/posts/NewPosts.vue"),
-    beforeEnter: () => {
-      const store = useUserStore();
-      if (store.getUser == "") return "/login";
-    },
+    beforeEnter: [isAuthenticated],
   },
   {
     path: "/posts/all",
     name: "AllPosts",
     component: () => import("../pages/posts/AllPosts.vue"),
-    beforeEnter: () => {
-      const store = useUserStore();
-      if (store.getUser == "") return "/login";
-    },
+    beforeEnter: [isAuthenticated],
   },
   {
     path: "/posts/:postId",
     name: "Post",
     component: () => import("../pages/posts/Post.vue"),
     props: true,
-    beforeEnter: () => {
-      const store = useUserStore();
-      if (store.getUser == "") return "/login";
-    },
+    beforeEnter: [isAuthenticated],
   },
   {
     path: "/card/:userId",
